46d0772d2b1632879852a43de7ef3b12da058010
jasim741891 <jasimgai74@gmail.com>
2021-08-26 22:46:44 +0600

    kanye arrow function

--- js/kanye.js
+++ js/kanye.js
@@ -1,1 +1,11 @@
-console.log('amar sonar bangla')
+// lesson-01
+const loadQuoe = () => {
+    fetch('https://api.kanye.rest/')
+        .then(res => res.json())
+        .then(data => displayQuote(data))
+}
+const displayQuote = quote => {
+    console.log(quote.quote)
+    const quoteElement = document.getElementById('quote')
+    quoteElement.innerText = quote.quote
+}
