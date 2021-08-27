9473ef1f07fa097e0dbde792156816b6ac94d04c
jasim741891 <jasimgai74@gmail.com>
2021-08-27 08:29:02 +0600

    random use api

--- js/kanye.js
+++ js/kanye.js
@@ -9,3 +9,24 @@ const displayQuote = quote => {
     const quoteElement = document.getElementById('quote')
     quoteElement.innerText = quote.quote
 }
+
+// lesson - 02
+
+const loadBuddys = () => {
+    fetch('https://randomuser.me/api/?results=5')
+        .then(res => res.json())
+        .then(data => displayBuddy(data))
+}
+loadBuddys()
+
+const displayBuddy = jamalas => {
+    const jasimData = jamalas.results
+    const div = document.getElementById('body')
+    for (const jamala of jasimData) {
+        const p = document.createElement('p')
+        p.innerText = `name: ${jamala.name.title} ${jamala.name.first} ${jamala.name.last
+            } `
+        div.appendChild(p)
+    }
+}
+
